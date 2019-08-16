0943257ab50a3d63be5df61992b4e78e9d59bc72
chenk <chenk@apical.com.cn>
2019-08-16 16:16:32 +0800

    优化代码，解决 partial 请求返回不正确的问题，优化内存使用

--- ffhttpd.c
+++ ffhttpd.c
@@ -34,10 +34,18 @@ static char* strlwr(char *s)
 #define FFHTTPD_SERVER_PORT     8080
 #define FFHTTPD_MAX_CONNECTION  10
 
-static char *g_ffhttpd_head =
+static char *g_ffhttpd_head1 =
 "HTTP/1.1 200 OK\r\n"
 "Server: ffhttpd/1.0.0\r\n"
 "Accept-Ranges: bytes\r\n"
+"Content-Type: %s\r\n"
+"Content-Length: %d\r\n"
+"Connection: close\r\n\r\n";
+
+static char *g_ffhttpd_head2 =
+"HTTP/1.1 206 Partial Content\r\n"
+"Server: ffhttpd/1.0.0\r\n"
+"Content-Range: bytes %d-%d/%d\r\n"
 "Content-Type: %s\r\n"
 "Content-Length: %d\r\n"
 "Connection: close\r\n\r\n";
@@ -55,7 +63,7 @@ static char *g_content_type_list[][2] = {
     { ".jpeg", "image/jpeg"                     },
     { ".jpg" , "image/jpeg"                     },
     { ".mp3" , "audio/mp3"                      },
-    { ".mp4" , "video/mpeg4"                    },
+    { ".mp4" , "video/mp4"                      },
     { ".pdf" , "application/pdf"                },
     { ".png" , "image/png"                      },
     { ".ppt" , "application/x-ppt"              },
@@ -85,29 +93,31 @@ static char* get_content_type(char *file)
     return "application/octet-stream";
 }
 
-static int get_file_length(char *file, int start, int end)
+static void get_file_range_size(char *file, int *start, int *end, int *size)
 {
-    int filesize;
     FILE *fp = fopen(file, "rb");
-    if (!fp) return 0;
-    fseek(fp, 0, SEEK_END);
-    filesize = ftell(fp) - start;
-    fclose(fp);
-    if (filesize < 0) filesize = 0;
-    return (filesize < end - start + 1 ? filesize : end - start + 1);
+    if (fp) {
+        fseek(fp, 0, SEEK_END);
+        *size = ftell(fp);
+        fclose(fp);
+        if (*start < 0) *start = *size + *start;
+        if (*end >= *size) *end = *size - 1;
+    } else {
+        *start = *end = *size = 0;
+    }
 }
 
-static void send_file_data(SOCKET fd, char *file, int start, int length)
+static void send_file_data(SOCKET fd, char *file, int start, int end)
 {
     FILE *fp = fopen(file, "rb");
     if (fp) {
         char buf[1024];
-        int  ret = 0, n;
+        int  len = end - start + 1, ret = 0, n;
         fseek(fp, start, SEEK_SET);
         do {
-            n = length < sizeof(buf) ? length : sizeof(buf);
+            n = len < sizeof(buf) ? len : sizeof(buf);
             n = (int)fread(buf, 1, n, fp);
-            length -= n > 0 ? n : 0;
+            len -= n > 0 ? n : 0;
             while (n > 0) {
                 ret = send(fd, buf, n, 0);
 #ifdef WIN32
@@ -117,80 +127,48 @@ static void send_file_data(SOCKET fd, char *file, int start, int length)
 #endif
                 n  -= ret > 0 ? ret : 0;
             }
-        } while (length > 0 && !feof(fp));
+        } while (len > 0 && !feof(fp));
 done:   fclose(fp);
     }
 }
 
-static void parse_type_and_path(char *str, char *type, int tsize, char *path, int psize)
+static void parse_range_datasize(char *str, int *partial, int *start, int *end, int *size)
 {
-    char *src_start, *src_end, *dst_end;
-    src_start = str;
-    src_end   = strstr(str, " ");
-    dst_end   = type + tsize;
-    if (src_start) {
-        while (src_start < src_end && type < dst_end) *type++ = *src_start++;
-    }
-    *type = 0;
-
-    src_start = strstr(str, "/");
-    dst_end   = path + psize;
-    if (src_start) {
-        src_start += 1;
-        src_end = strstr(src_start, " ");
-        while (src_start < src_end && path < dst_end) *path++ = *src_start++;
-        if (path == dst_end) path--;
-    }
-    *path = 0;
-}
-
-static void parse_range(char *str, int *start, int *end)
-{
-    char *header_start, *header_end, *header_buf, *range_start, *range_end;
-    *start       = 0;
-    *end         = 0x7FFFFFFE;
-    header_start = strstr(str, "\r\n");
-    header_end   = strstr(str, "\r\n\r\n");
-    if (!header_start || !header_end) return;
-    header_start+= 2;
-    header_end  += 2;
-    header_buf   = malloc(header_end - header_start + 1);
-    if (header_buf) {
-        memcpy(header_buf, header_start, header_end - header_start);
-        header_buf[header_end - header_start] = 0;
-        strlwr(header_buf);
-        range_start = strstr(header_buf, "range");
-        if (range_start && (range_end = strstr(range_start, "\r\n"))) {
-            if (strstr(range_start, ":") && strstr(range_start, "bytes") && (range_start = strstr(range_start, "="))) {
+    char *range_start, *range_end, *temp;
+    *start = 0;
+    *end   = 0x7FFFFFFF;
+    *size  = 0;
+    range_start = strstr(str, "range");
+    if (range_start && (range_end = strstr(range_start, "\r\n"))) {
+        if (strstr(range_start, ":") && strstr(range_start, "bytes") && (range_start = strstr(range_start, "="))) {
+            range_start += 1;
+            *start = atoi(range_start);
+            if (*start < 0) {
+                range_start  = strstr(range_start, "-");
                 range_start += 1;
-               *start = atoi(range_start);
-                if (*start < 0) {
-                    range_start = strstr(range_start, "-");
-                    range_start+= 1;
-                }
-                range_start = strstr(range_start, "-");
-                if (range_start && range_start + 1 < range_end) {
-                    range_start += 1;
-                    *end = atoi(range_start);
-                }
+            }
+            range_start = strstr(range_start, "-");
+            if (range_start && range_start + 1 < range_end) {
+                range_start += 1;
+                *end = atoi(range_start);
             }
         }
-        free(header_buf);
     }
+    temp = strstr(str, "content-length");
+    if (temp) {
+        temp += 14;
+        temp  = strstr(temp, ":");
+        if (temp) *size = atoi(temp+1);
+    }
+    *partial = !!range_start;
 }
 
 typedef struct {
     SOCKET          fd;
-    char            type[5];
-    char            recvbuf[1024];
-    char            sendbuf[1024];
-    char            path[1024];
-    char           *args;
-    char           *data;
     pthread_t       thread;
     pthread_mutex_t mutex;
     pthread_cond_t  cond;
-    #define CONN_STATUS_RUNNING (1 << 0)
+    #define CONN_STATUS_WORKING (1 << 0)
     #define CONN_STATUS_EXIT    (1 << 1)
     int             status;
 } CONN;
@@ -199,54 +177,70 @@ static CONN g_conn_pool[FFHTTPD_MAX_CONNECTION] = {0};
 static void* handle_http_request(void *argv)
 {
     CONN *conn = (CONN*)argv;
-    int length, range_start, range_end, exitflag = 0;
+    int   range_start, range_end, datasize, partial, exitflag = 0;
+    char *request_line, *request_header, *request_data = NULL, *request_type = NULL, *request_path = NULL, *url_args = NULL;
+    char  recvbuf[1024], sendbuf[1024];
 
-    while (1) {
+    while (!exitflag) {
         pthread_mutex_lock(&conn->mutex);
-        while (!(conn->status & (CONN_STATUS_EXIT|CONN_STATUS_RUNNING))) pthread_cond_wait(&conn->cond, &conn->mutex);
-        if ((conn->status & CONN_STATUS_EXIT) && conn->fd >= 0) {
-            if (conn->fd != -1) {
-                closesocket(conn->fd);
-                conn->fd = -1;
-            }
-            exitflag = 1;
-        }
+        while (!(conn->status & (CONN_STATUS_EXIT|CONN_STATUS_WORKING))) pthread_cond_wait(&conn->cond, &conn->mutex);
+        if (conn->status & CONN_STATUS_EXIT) exitflag = 1;
         pthread_mutex_unlock(&conn->mutex);
-        if (exitflag) break;
+        if (exitflag) goto next;
 
-        length = recv(conn->fd, conn->recvbuf, sizeof(conn->recvbuf), 0);
-        conn->recvbuf[length] = 0;
-        conn->data = strstr(conn->recvbuf, "\r\n\r\n");
-        conn->data = conn->data ? conn->data + 4 : NULL;
-        printf("request :\n%s\n", conn->recvbuf); fflush(stdout);
-        parse_type_and_path(conn->recvbuf, conn->type, sizeof(conn->type), conn->path, sizeof(conn->path));
-        parse_range(conn->recvbuf, &range_start, &range_end);
-        if (conn->path[0]) {
-            conn->args = strstr(conn->path, "?");
-            if (conn->args) *conn->args++ = 0;
-        } else {
-            strcpy(conn->path, "index.html");
+        datasize = recv(conn->fd, recvbuf, sizeof(recvbuf), 0);
+        recvbuf[datasize < sizeof(recvbuf) ? datasize : sizeof(recvbuf) - 1] = 0;
+        printf("request :\n%s\n", recvbuf); fflush(stdout);
+
+        request_line   = recvbuf;
+        request_header = strstr(recvbuf, "\r\n");
+        if (request_header) {
+            request_header[0] = 0;
+            request_header   += 2;
+            strlwr(request_header);
+            request_data = strstr(request_header, "\r\n\r\n");
+            if (request_data) {
+                request_data[0] = 0;
+                request_data   += 4;
+            }
         }
-        printf("conn->type: %s, conn->path: %s, conn->args: %s, conn->data: %s, range_start: %d, range_end: %d\n\n",
-                conn->type, conn->path, conn->args, conn->data, range_start, range_end); fflush(stdout);
-
-        if (strcmp(conn->type, "GET") == 0 || strcmp(conn->type, "HEAD") == 0) {
-            length = get_file_length(conn->path, range_start, range_end);
-            snprintf(conn->sendbuf, sizeof(conn->sendbuf), g_ffhttpd_head, get_content_type(conn->path), length);
-            printf("response:\n%s\n", conn->sendbuf); fflush(stdout);
-            send(conn->fd, conn->sendbuf, (int)strlen(conn->sendbuf), 0);
-            if (length > 0 && strcmp(conn->type, "GET") == 0) send_file_data(conn->fd, conn->path, range_start, length);
-        } else if (strcmp(conn->type, "POST") == 0 && conn->data) {
-            snprintf(conn->sendbuf, sizeof(conn->sendbuf), g_ffhttpd_head, "text/plain", 0);
-            send(conn->fd, conn->sendbuf, (int)strlen(conn->sendbuf), 0);
+        request_type = request_line;
+        request_path = strstr(request_line, " ");
+        if (request_path) *request_path++ = 0;
+        request_path = strstr(request_path, "/");
+        if (request_path++) {
+            url_args = strstr(request_path, " ");
+            if (url_args) *url_args   = 0;
+            url_args = strstr(request_path, "?");
+            if (url_args) *url_args++ = 0;
+            if (!request_path[0]) request_path = "index.html";
         }
 
-        pthread_mutex_lock(&conn->mutex);
+        parse_range_datasize(request_header, &partial, &range_start, &range_end, &datasize);
+        printf("request_type: %s, request_path: %s, url_args: %s, request_data: %s\n", request_type, request_path, url_args, request_data); fflush(stdout);
+
+        if (strcmp(request_type, "GET") == 0 || strcmp(request_type, "HEAD") == 0) {
+            get_file_range_size(request_path, &range_start, &range_end, &datasize);
+            if (!partial) {
+                snprintf(sendbuf, sizeof(sendbuf), g_ffhttpd_head1, get_content_type(request_path), datasize);
+            } else {
+                snprintf(sendbuf, sizeof(sendbuf), g_ffhttpd_head2, range_start, range_end, datasize, get_content_type(request_path), range_end - range_start + 1);
+            }
+            printf("response:\n%s\n", sendbuf); fflush(stdout);
+            send(conn->fd, sendbuf, (int)strlen(sendbuf), 0);
+            if (strcmp(request_type, "GET") == 0) send_file_data(conn->fd, request_path, range_start, range_end);
+        } else if (strcmp(request_type, "POST") == 0) {
+            snprintf(sendbuf, sizeof(sendbuf), g_ffhttpd_head1, "text/plain", 0);
+            send(conn->fd, sendbuf, (int)strlen(sendbuf), 0);
+//          printf("\nhttp post request\npath = %s, args = %s, data length = %d, data buffer = %s\n\n", request_path, url_args, datasize, request_data);
+        }
+
+next:   pthread_mutex_lock(&conn->mutex);
         if (conn->fd != -1) {
             closesocket(conn->fd);
             conn->fd = -1;
         }
-        conn->status &= ~CONN_STATUS_RUNNING;
+        conn->status &= ~CONN_STATUS_WORKING;
         pthread_mutex_unlock(&conn->mutex);
     }
     return NULL;
@@ -279,8 +273,8 @@ static void conn_pool_run(CONN *pool, int n, SOCKET connfd)
     int flag = 0, i;
     for (i=0; i<n&&!flag; i++) {
         pthread_mutex_lock(&pool[i].mutex);
-        if (!(pool[i].status & CONN_STATUS_RUNNING)) {
-            pool[i].status |= CONN_STATUS_RUNNING;
+        if (!(pool[i].status & CONN_STATUS_WORKING)) {
+            pool[i].status |= CONN_STATUS_WORKING;
             pool[i].fd = connfd;
             pthread_cond_signal(&pool[i].cond);
             flag = 1;
